365a8c71d643c8b90e7af45b441a3150a6d50169
Alex Hool <alexhool97@gmail.com>
2023-06-09 15:54:06 -0700

    finished

--- Ambient Light Detector/bar_gui.py
+++ Ambient Light Detector/bar_gui.py
@@ -1,26 +1,41 @@
 import matplotlib
-matplotlib.use('qtagg')
-from matplotlib import animation
+matplotlib.use('QtAgg')
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
+import serial
 
-lst=[i for i in (np.sin(np.linspace(-np.pi, np.pi, 200))+10)]
+ser = serial.Serial(
+        port='COM3', 
+        baudrate=9600, 
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=0)
 
 fig = plt.figure(figsize=(4,6))
-axes = fig.add_subplot(1,1,1)
-plt.subplots_adjust(top=0.87, bottom=0.11, left=0.15, right=0.9, hspace=0.2, wspace=0.2)
-plt.tick_params(axis='x', bottom=False, labelsize=12, pad=10)
-
+fig.set_facecolor('#e4f2f5')
+ax = fig.add_subplot(1,1,1)
+ax.set_facecolor('#f0f6f7')
+plt.subplots_adjust(top=0.865, bottom=0.115, left=0.215, right=0.88, hspace=0.2, wspace=0.2)
+plt.tick_params(axis='x', bottom=False, labelsize=14, pad=10)
+plt.tick_params(axis='y', left=True, labelsize=12, pad=2)
 
 def animate(i):
-    axes.clear()
-    axes.margins(0.5, 0)
-    axes.set_ylim(min(lst), max(lst)+1)
-    plt.yticks(np.arange(min(lst), max(lst)+1, step=0.5))
+    while True:
+        try:
+            voltage = float(ser.readline().decode('utf-8')) * (5.0 / 1023.0)
+            if (voltage > 0.5 or voltage == 0.0):
+                break
+        except:
+            pass
+    ax.clear()
+    ax.margins(0.5, 0)
+    ax.set_ylim(0, 5.25)
     plt.title("Ambient Light", size=16, pad=20, weight='bold')
-    plt.bar("Brightness", lst[i], color='#a1c9f4', linewidth=0, width=2, align='center')
+    plt.ylabel("Volts", size=14, labelpad=8)
+    plt.yticks(np.arange(0, 5.5, step=0.5))
+    plt.bar("Brightness", voltage, color='#a1c9f4', linewidth=0, width=2, align='center')
 
-ani = FuncAnimation(fig, animate, frames=200, interval=1, repeat=True)
-print(len(lst))
+ani = FuncAnimation(fig, animate, frames=1000, interval=0, repeat=True)
 plt.show()
