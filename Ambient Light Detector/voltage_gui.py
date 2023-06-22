307efd61e87bb58e198aefb9ffde65e91294e32f
Alex Hool <alexhool97@gmail.com>
2023-06-22 14:25:01 -0700

    update voltage calc

--- Ambient Light Detector/voltage_gui.py
+++ Ambient Light Detector/voltage_gui.py
@@ -152,7 +152,7 @@ while True:
     if lst:
         lst.pop()
     try:
-        VOLTAGE = int("".join(lst)) * (4.964 / 1023.0)
+        VOLTAGE = int("".join(lst)) * (4.96 / 1023.0)
         update(VOLTAGE)
     except ValueError:
         pass
