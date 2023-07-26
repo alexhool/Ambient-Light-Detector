6ea8d16ae9ab7f93956ca82d285edb2251100aa1
Alex Hool <64939304+alexhool@users.noreply.github.com>
2023-07-26 15:51:19 -0700

    added night led to arduino

--- Ambient Light Detector/serial_voltage/serial_voltage.ino
+++ Ambient Light Detector/serial_voltage/serial_voltage.ino
@@ -1,4 +1,6 @@
 bool ledOn = false;
+bool nightOn = false;
+bool stateOn = false;
 
 void setup() {
   Serial.begin(9600);
@@ -7,11 +9,12 @@ void setup() {
 }
 
 void loop() {
+  long int t1 = micros();
   int value = analogRead(A7);
-  Serial.print(value);
-  Serial.flush();
-  Serial.print('|');
-  Serial.flush();
+  //Serial.print(value);
+  //Serial.flush();
+  //Serial.print('|');
+  //Serial.flush();
   if (Serial.available() > 0) {
     int incomingByte = Serial.read();
     if (incomingByte == 'H') {
@@ -19,6 +22,12 @@ void loop() {
     }
     if (incomingByte == 'L') {
       ledOn = false;
+    }
+    if (incomingByte == 'N') {
+      nightOn = true;
+    }
+    if (incomingByte == 'D') {
+      nightOn = false;
     }
   }
   if (ledOn) {
@@ -35,8 +44,26 @@ void loop() {
       digitalWrite(2, LOW);
       delayMicroseconds(11410 - delay); 
     }
+  } else if (nightOn) {
+    if (value >= 206) {
+      stateOn = false;
+      digitalWrite(2, LOW);
+    } else if (value <= 103) {
+      stateOn = true;
+      digitalWrite(2, HIGH);
+    } else {
+      if (stateOn) {
+        digitalWrite(2, HIGH);
+      } else {
+        digitalWrite(2, LOW);
+      }
+    }
+    delayMicroseconds(11463);
   } else {
     digitalWrite(2, LOW);
     delayMicroseconds(11465);
   }
+  long int t2 = micros();
+  Serial.println(t2-t1);
+  Serial.flush();
 }
