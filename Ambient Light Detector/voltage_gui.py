d3f213523eadf178d42a1acb4fbaf4e67949c8a8
Alex Hool <64939304+alexhool@users.noreply.github.com>
2023-07-26 10:39:45 -0700

    convert to importable module

--- Ambient Light Detector/voltage_gui.py
+++ Ambient Light Detector/voltage_gui.py
@@ -7,258 +7,259 @@ import serial
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
-win = psutil.Process()
-win.nice(psutil.HIGH_PRIORITY_CLASS)
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Function to turn the LED on
-def led_on():
-    onLED.configure(state="disabled", bg="#D3D3D3")
-    ser.write(bytes("H", "UTF-8"))
-    ser.flush()
-    offLED.configure(state="normal", bg="#f0f6f7")
-
-
-# Function to turn the LED off
-def led_off():
-    try:
-        ser.write(bytes("L", "UTF-8"))
+def main():
+    # Function to turn the LED on
+    def led_on():
+        onLED.configure(state="disabled", bg="#D3D3D3")
+        ser.write(bytes("H", "UTF-8"))
         ser.flush()
-    except serial.SerialException:
-        pass
-    offLED.configure(state="disabled", bg="#D3D3D3")
-    onLED.configure(state="normal", bg="#f0f6f7")
+        offLED.configure(state="normal", bg="#f0f6f7")
 
-
-# Function to turn serial connection on
-def serial_on():
-    try:
-        onSer.configure(state="disabled", bg="#D3D3D3")
-        rect[0].set_visible(True)
-        ser.open()
-        time.sleep(1.27)
+    # Function to turn the LED off
+    def led_off():
+        try:
+            ser.write(bytes("L", "UTF-8"))
+            ser.flush()
+        except serial.SerialException:
+            pass
+        offLED.configure(state="disabled", bg="#D3D3D3")
         onLED.configure(state="normal", bg="#f0f6f7")
-        offSer.configure(state="normal", bg="#f0f6f7")
-    except serial.SerialException:
-        pass
 
-
-# Function to turn serial connection off
-def serial_off():
-    offSer.configure(state="disabled", bg="#D3D3D3")
-    led_off()
-    onLED.configure(state="disabled", bg="#D3D3D3")
-    ser.close()
-    rect[0].set_visible(False)
-    text.set_text("Lost Connection")
-    text.set_y(2.7)
-    textLux.config(text="")
-    canvas.draw()
-    onSer.configure(state="normal", bg="#f0f6f7")
-
-
-# Function to update the bar graph
-def update(analog):
-    lux = int(round(pow(analog, 2) * -0.00035 + analog * 1.85, -1))
-    height = analog * (4.963 / 1023.0)
-    ax.margins(0.5, 0)
-    ax.set_ylim(0, 5.4)
-    ax.set_yticks([0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5])
-    ax.set_title("Ambient Light", size=16, pad=25, weight="bold")
-    ax.set_ylabel("Voltage (V)", size=14, labelpad=10)
-    rect[0].set_height(height)
-    text.set_text(f"{height:.3f} V")
-    text.set_y(height + 0.08)
-    textLux.config(text=f"{lux} Lux")
-    canvas.draw()
-
-
-# Function to quit the program
-def exit_gui():
-    try:
-        if not ser.is_open:
+    # Function to turn serial connection on
+    def serial_on():
+        try:
+            onSer.configure(state="disabled", bg="#D3D3D3")
+            rect[0].set_visible(True)
             ser.open()
+            time.sleep(1.27)
+            onLED.configure(state="normal", bg="#f0f6f7")
+            offSer.configure(state="normal", bg="#f0f6f7")
+        except serial.SerialException:
+            pass
+
+    # Function to turn serial connection off
+    def serial_off():
+        offSer.configure(state="disabled", bg="#D3D3D3")
         led_off()
-    except serial.SerialException:
-        pass
-    os._exit(0)
+        onLED.configure(state="disabled", bg="#D3D3D3")
+        ser.close()
+        rect[0].set_visible(False)
+        text.set_text("Lost Connection")
+        text.set_y(2.7)
+        textLux.config(text="")
+        canvas.draw()
+        onSer.configure(state="normal", bg="#f0f6f7")
 
+    # Function to update the bar graph
+    def update(analog):
+        lux = int(round(pow(analog, 2) * -0.00035 + analog * 1.85, -1))
+        height = analog * (4.963 / 1023.0)
+        ax.margins(0.5, 0)
+        ax.set_ylim(0, 5.4)
+        ax.set_yticks([0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5])
+        ax.set_title("Ambient Light", size=16, pad=25, weight="bold")
+        ax.set_ylabel("Voltage (V)", size=14, labelpad=10)
+        rect[0].set_height(height)
+        text.set_text(f"{height:.3f} V")
+        text.set_y(height + 0.08)
+        textLux.config(text=f"{lux} Lux")
+        canvas.draw()
 
-# Initialize serial connection
-ser = serial.Serial(baudrate=9600, timeout=0)
-ser.port = "COM3"
+    # Function to quit the program
+    def exit_gui():
+        try:
+            if not ser.is_open:
+                ser.open()
+            led_off()
+        except serial.SerialException:
+            pass
+        os._exit(0)
 
-# Set up the Tkinter GUI
-root = tk.Tk()
-root.title("Ambient Light Graph")
-icon = tk.PhotoImage(file="light-bulb.png")
-root.iconphoto(True, icon)
-root.geometry("543x625")
-root.resizable(False, False)
-root.config(bg="#fdefc3")
+    # Set the process priority to high
+    win = psutil.Process()
+    win.nice(psutil.HIGH_PRIORITY_CLASS)
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# Set up the plot
-fig = Figure(figsize=(4, 6), facecolor="#fdefc3")
-fig.subplots_adjust(
-    top=0.865, bottom=0.115, left=0.215, right=0.85, hspace=0.2, wspace=0.2
-)
-ax = fig.add_subplot(1, 1, 1)
-ax.set_facecolor("#fefdf9")
-ax.tick_params(axis="x", bottom=False, labelsize=14, pad=10)
-ax.tick_params(axis="y", left=True, labelsize=12, pad=2)
-rect = ax.bar(
-    x="Brightness",
-    height=0,
-    color="#fcd768",
-    edgecolor="#000000",
-    linewidth=0.7,
-    width=2,
-    align="center",
-)
-text = ax.text(0, 0, "", ha="center", va="bottom", size=11)
+    # Initialize serial connection
+    ser = serial.Serial(baudrate=9600, timeout=0)
+    ser.port = "COM3"
 
-# Add the plot to the Tkinter widget
-canvas = FigureCanvasTkAgg(fig, root)
-canvas.get_tk_widget().config(bg="#000000")
-canvas.get_tk_widget().grid(
-    column=0, row=0, rowspan=15, sticky=tk.NSEW, padx=10, pady=10, ipadx=5, ipady=2
-)
+    # Set up the Tkinter GUI
+    root = tk.Tk()
+    root.title("Ambient Light Graph")
+    icon = tk.PhotoImage(file="light-bulb.png")
+    root.iconphoto(True, icon)
+    root.geometry("543x625")
+    root.resizable(False, False)
+    root.config(bg="#fdefc3")
 
-# Add Serial Connection label
-labelSer = tk.Label(
-    root,
-    text="Serial Connection",
-    font=("DejaVu Sans", 15),
-    bg="#fdefc3",
-    wraplength=110,
-    justify="center",
-)
-labelSer.grid(column=1, row=3, padx=1, pady=0, sticky=tk.SW)
+    # Set up the plot
+    fig = Figure(figsize=(4, 6), facecolor="#fdefc3")
+    fig.subplots_adjust(
+        top=0.865, bottom=0.115, left=0.215, right=0.85, hspace=0.2, wspace=0.2
+    )
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_facecolor("#fefdf9")
+    ax.tick_params(axis="x", bottom=False, labelsize=14, pad=10)
+    ax.tick_params(axis="y", left=True, labelsize=12, pad=2)
+    rect = ax.bar(
+        x="Brightness",
+        height=0,
+        color="#fcd768",
+        edgecolor="#000000",
+        linewidth=0.7,
+        width=2,
+        align="center",
+    )
+    text = ax.text(0, 0, "", ha="center", va="bottom", size=11)
 
-# Add the Serial On button
-onSer = tk.Button(
-    root,
-    text="On",
-    width=10,
-    height=1,
-    font=("DejaVu Sans", 9),
-    bg="#D3D3D3",
-    state="disabled",
-    command=serial_on,
-)
-onSer.grid(column=1, row=4, padx=12, pady=10, sticky=tk.NW)
+    # Add the plot to the Tkinter widget
+    canvas = FigureCanvasTkAgg(fig, root)
+    canvas.get_tk_widget().config(bg="#000000")
+    canvas.get_tk_widget().grid(
+        column=0, row=0, rowspan=15, sticky=tk.NSEW, padx=10, pady=10, ipadx=5, ipady=2
+    )
 
-# Add the Serial Off button
-offSer = tk.Button(
-    root,
-    text="Off",
-    width=10,
-    height=1,
-    font=("DejaVu Sans", 9),
-    bg="#fefdf9",
-    state="normal",
-    command=serial_off,
-)
-offSer.grid(column=1, row=4, padx=12, pady=38, sticky=tk.NW)
+    # Add Serial Connection label
+    labelSer = tk.Label(
+        root,
+        text="Serial Connection",
+        font=("DejaVu Sans", 15),
+        bg="#fdefc3",
+        wraplength=110,
+        justify="center",
+    )
+    labelSer.grid(column=1, row=3, padx=1, pady=0, sticky=tk.SW)
 
-# Add LED Visualizer label
-labelLED = tk.Label(
-    root,
-    text="LED Visualizer",
-    font=("DejaVu Sans", 15),
-    bg="#fdefc3",
-    wraplength=100,
-    justify="center",
-)
-labelLED.grid(column=1, row=5, padx=8, pady=0, sticky=tk.SW)
+    # Add the Serial On button
+    onSer = tk.Button(
+        root,
+        text="On",
+        width=10,
+        height=1,
+        font=("DejaVu Sans", 9),
+        bg="#D3D3D3",
+        state="disabled",
+        command=serial_on,
+    )
+    onSer.grid(column=1, row=4, padx=12, pady=10, sticky=tk.NW)
 
-# Add the LED On button
-onLED = tk.Button(
-    root,
-    text="On",
-    width=10,
-    height=1,
-    font=("DejaVu Sans", 9),
-    bg="#fefdf9",
-    state="normal",
-    command=led_on,
-)
-onLED.grid(column=1, row=6, padx=12, pady=10, sticky=tk.NW)
+    # Add the Serial Off button
+    offSer = tk.Button(
+        root,
+        text="Off",
+        width=10,
+        height=1,
+        font=("DejaVu Sans", 9),
+        bg="#fefdf9",
+        state="normal",
+        command=serial_off,
+    )
+    offSer.grid(column=1, row=4, padx=12, pady=38, sticky=tk.NW)
 
-# Add the LED Off button
-offLED = tk.Button(
-    root,
-    text="Off",
-    width=10,
-    height=1,
-    font=("DejaVu Sans", 9),
-    bg="#D3D3D3",
-    state="disabled",
-    command=led_off,
-)
-offLED.grid(column=1, row=6, padx=12, pady=38, sticky=tk.NW)
+    # Add LED Visualizer label
+    labelLED = tk.Label(
+        root,
+        text="LED Visualizer",
+        font=("DejaVu Sans", 15),
+        bg="#fdefc3",
+        wraplength=100,
+        justify="center",
+    )
+    labelLED.grid(column=1, row=5, padx=8, pady=0, sticky=tk.SW)
 
-# Add the Lux label
-labelLux = tk.Label(
-    root,
-    text="Incident Light Value",
-    font=("DejaVu Sans", 15),
-    bg="#fdefc3",
-    wraplength=100,
-    justify="center",
-)
-labelLux.grid(column=1, row=7, padx=1, pady=0, sticky=tk.SW)
+    # Add the LED On button
+    onLED = tk.Button(
+        root,
+        text="On",
+        width=10,
+        height=1,
+        font=("DejaVu Sans", 9),
+        bg="#fefdf9",
+        state="normal",
+        command=led_on,
+    )
+    onLED.grid(column=1, row=6, padx=12, pady=10, sticky=tk.NW)
 
-# Add the Lux value label
-textLux = tk.Label(
-    root,
-    width=11,
-    height=1,
-    font=("DejaVu Sans", 13),
-    bg="#fefdf9",
-    borderwidth=1,
-    relief="solid",
-    anchor=tk.CENTER,
-)
-textLux.grid(column=1, row=8, padx=1, pady=11, ipady=16, sticky=tk.NW)
+    # Add the LED Off button
+    offLED = tk.Button(
+        root,
+        text="Off",
+        width=10,
+        height=1,
+        font=("DejaVu Sans", 9),
+        bg="#D3D3D3",
+        state="disabled",
+        command=led_off,
+    )
+    offLED.grid(column=1, row=6, padx=12, pady=38, sticky=tk.NW)
 
-# Add the quit button
-quitB = tk.Button(
-    root,
-    text="QUIT",
-    width=10,
-    height=2,
-    font=("DejaVu Sans", 9),
-    bg="#fefdf9",
-    activebackground="#de282c",
-    activeforeground="#fefdf9",
-    command=exit_gui,
-)
-quitB.grid(column=1, row=14, padx=12, pady=8, sticky=tk.W)
-root.protocol("WM_DELETE_WINDOW", exit_gui)
+    # Add the Lux label
+    labelLux = tk.Label(
+        root,
+        text="Incident Light Value",
+        font=("DejaVu Sans", 15),
+        bg="#fdefc3",
+        wraplength=100,
+        justify="center",
+    )
+    labelLux.grid(column=1, row=7, padx=1, pady=0, sticky=tk.SW)
 
-# Read serial data and update the bar graph
-lst = []
-update(0)
-try:
-    serial_on()
-except serial.SerialException:
-    pass
-while True:
+    # Add the Lux value label
+    textLux = tk.Label(
+        root,
+        width=11,
+        height=1,
+        font=("DejaVu Sans", 13),
+        bg="#fefdf9",
+        borderwidth=1,
+        relief="solid",
+        anchor=tk.CENTER,
+    )
+    textLux.grid(column=1, row=8, padx=1, pady=11, ipady=16, sticky=tk.NW)
+
+    # Add the quit button
+    quitB = tk.Button(
+        root,
+        text="QUIT",
+        width=10,
+        height=2,
+        font=("DejaVu Sans", 9),
+        bg="#fefdf9",
+        activebackground="#de282c",
+        activeforeground="#fefdf9",
+        command=exit_gui,
+    )
+    quitB.grid(column=1, row=14, padx=12, pady=8, sticky=tk.W)
+    root.protocol("WM_DELETE_WINDOW", exit_gui)
+
+    # Read serial data and update the bar graph
+    lst = []
+    update(0)
     try:
-        while "|" not in lst:
-            data = ser.read().decode().strip()
-            if data:
-                lst.append(data)
-        if lst:
-            lst.pop()
-        analog = int("".join(lst))
-        update(analog)
-    except ValueError:
-        pass
+        serial_on()
     except serial.SerialException:
-        serial_off()
-    lst.clear()
-    root.update_idletasks()
-    root.update()
+        pass
+    while True:
+        try:
+            while "|" not in lst:
+                data = ser.read().decode().strip()
+                if data:
+                    lst.append(data)
+            if lst:
+                lst.pop()
+            analog = int("".join(lst))
+            update(analog)
+        except ValueError:
+            pass
+        except serial.SerialException:
+            serial_off()
+        lst.clear()
+        root.update_idletasks()
+        root.update()
+
+
+# Run the program
+if __name__ == "__main__":
+    main()
